3b7b5cb8591c1f13bb035f533292cbfa45579ec0
fsoich-redstage <74548121+fsoich-redstage@users.noreply.github.com>
2025-07-03 18:32:25 -0300

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -14,58 +14,68 @@ overrideGQLOperations([
   {
     npm: '@dropins/storefront-checkout',
     operations: [
-      `
-      fragment CHECKOUT_DATA_FRAGMENT on Cart {
-        available_payment_methods {
-          code
-          title
-          oope_payment_method_config {
-            backend_integration_url
-            custom_config {
-              ... on CustomConfigKeyValue {
-                key
-                value
+      {
+        operationName: 'getCart',
+        operationType: 'query',
+        gql: `
+          query getCart($cartId: String!) {
+            cart(cart_id: $cartId) {
+              id
+              email
+              is_virtual
+              total_quantity
+              available_payment_methods {
+                code
+                title
+                oope_payment_method_config {
+                  backend_integration_url
+                  custom_config {
+                    ... on CustomConfigKeyValue {
+                      key
+                      value
+                    }
+                  }
+                }
+              }
+              selected_payment_method {
+                code
+                title
+                oope_payment_method_config {
+                  backend_integration_url
+                  custom_config {
+                    ... on CustomConfigKeyValue {
+                      key
+                      value
+                    }
+                  }
+                }
+              }
+              shipping_addresses {
+                selected_shipping_method {
+                  carrier_code
+                  method_code
+                  carrier_title
+                  method_title
+                  amount {
+                    value
+                    currency
+                  }
+                }
+                available_shipping_methods {
+                  carrier_code
+                  method_code
+                  carrier_title
+                  method_title
+                  amount {
+                    value
+                    currency
+                  }
+                }
               }
             }
           }
-        }
-        selected_payment_method {
-          code
-          title
-          oope_payment_method_config {
-            backend_integration_url
-            custom_config {
-              ... on CustomConfigKeyValue {
-                key
-                value
-              }
-            }
-          }
-        }
-        shipping_addresses {
-          selected_shipping_method {
-            carrier_code
-            method_code
-            carrier_title
-            method_title
-            amount {
-              value
-              currency
-            }
-          }
-          available_shipping_methods {
-            carrier_code
-            method_code
-            carrier_title
-            method_title
-            amount {
-              value
-              currency
-            }
-          }
-        }
+        `
       }
-      `
     ],
   },
 ]);
