86d392d80f441b7330669bc9e8087c37165044c8
fsoich-redstage <74548121+fsoich-redstage@users.noreply.github.com>
2025-07-04 10:09:39 -0300

    Update checkout.js

--- scripts/initializers/checkout.js
+++ scripts/initializers/checkout.js
@@ -6,14 +6,14 @@ import { fetchPlaceholders } from '../commerce.js';
 import { events } from '@dropins/tools/event-bus.js';
 
 function handleCheckoutInitialized(data) {
-  console.log('[checkout.js] 🛒 Evento: cart/initialized AAA');
-  console.log('[checkout.js] 💳 Métodos de pago:', data?.availablePaymentMethods, 'AAA');
-  console.log('[checkout.js] 🚚 Métodos de envío:', data?.shipping_addresses?.[0]?.available_shipping_methods, 'AAA');
+  console.log('[checkout.js] 🛒 Evento: cart/initialized BBB');
+  console.log('[checkout.js] 💳 Métodos de pago:', data?.availablePaymentMethods, 'BBB');
+  console.log('[checkout.js] 🚚 Métodos de envío:', data?.shipping_addresses?.[0]?.available_shipping_methods, 'BBB');
 }
 
 function handleCartData(data) {
-  console.log('[checkout.js] 🛒 Evento: cart/data AAA');
-  console.log('[checkout.js] 🛍️ CartModel.transformer recibió:', data, 'AAA');
+  console.log('[checkout.js] 🛒 Evento: cart/data BBB');
+  console.log('[checkout.js] 🛍️ CartModel.transformer recibió:', data, 'BBB');
 }
 
 events.on('checkout/initialized', handleCheckoutInitialized, { eager: true });
@@ -34,15 +34,13 @@ await initializeDropin(async () => {
     models: {
       CartModel: {
         transformer: (data) => {
-          console.log('[checkout.js] 🛒 CartModel.transformer recibió:', data, 'AAA');
-
-          const shippingAddress = data?.shipping_addresses?.[0] || {};
-
+          const shipping = data?.shipping_addresses?.[0] || {};
+          console.log('[checkout.js] 🛒 CartModel.transformer recibió:', data, 'BBB');
           return {
             availablePaymentMethods: data?.available_payment_methods ?? [],
             selectedPaymentMethod: data?.selected_payment_method ?? null,
-            availableShippingMethods: shippingAddress.available_shipping_methods ?? [],
-            selectedShippingMethod: shippingAddress.selected_shipping_method ?? null,
+            availableShippingMethods: shipping?.available_shipping_methods ?? [],
+            selectedShippingMethod: shipping?.selected_shipping_method ?? null,
             shippingAddresses: data?.shipping_addresses ?? [],
           };
         },
