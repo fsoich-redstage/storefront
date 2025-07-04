422d026d004dc436e794c6fb67a33a76cca01433
fsoich-redstage <74548121+fsoich-redstage@users.noreply.github.com>
2025-07-03 21:21:05 -0300

    Update checkout.js

--- scripts/initializers/checkout.js
+++ scripts/initializers/checkout.js
@@ -6,14 +6,14 @@ import { fetchPlaceholders } from '../commerce.js';
 import { events } from '@dropins/tools/event-bus.js';
 
 function handleCheckoutInitialized(data) {
-  console.log('[checkout.js] 🛒 Evento: cart/initialized FEDE');
-  console.log('[checkout.js] 💳 Métodos de pago:', data?.availablePaymentMethods, 'FEDE');
-  console.log('[checkout.js] 🚚 Métodos de envío:', data?.shipping_addresses?.[0]?.available_shipping_methods, 'FEDE');
+  console.log('[checkout.js] 🛒 Evento: cart/initialized ZZZ');
+  console.log('[checkout.js] 💳 Métodos de pago:', data?.availablePaymentMethods, 'ZZZ');
+  console.log('[checkout.js] 🚚 Métodos de envío:', data?.shipping_addresses?.[0]?.available_shipping_methods, 'ZZZ');
 }
 
 function handleCartData(data) {
-  console.log('[checkout.js] 🛒 Evento: cart/data FEDE');
-  console.log('[checkout.js] 🛍️ CartModel.transformer recibió:', data, 'FEDE');
+  console.log('[checkout.js] 🛒 Evento: cart/data ZZZ');
+  console.log('[checkout.js] 🛍️ CartModel.transformer recibió:', data, 'ZZZ');
 }
 
 events.on('checkout/initialized', handleCheckoutInitialized, { eager: true });
@@ -34,7 +34,7 @@ await initializeDropin(async () => {
     models: {
       CartModel: {
         transformer: (data) => {
-          console.log('[checkout.js] 🛒 CartModel.transformer recibió:', data, 'FEDE');
+          console.log('[checkout.js] 🛒 CartModel.transformer recibió:', data, 'ZZZ');
           return {
             availablePaymentMethods: data?.available_payment_methods,
             selectedPaymentMethod: data?.selected_payment_method,
